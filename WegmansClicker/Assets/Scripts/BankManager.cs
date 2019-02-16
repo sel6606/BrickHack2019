39e25fb90c984146687b0c5c3d43a6d3b02facc4
LN <ln4269@rit.edu>
2019-02-16 15:45:42 -0500

    Add Bank, Meat and Dairy

---
+++ WegmansClicker/Assets/Scripts/BankManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BankManager : MonoBehaviour {
+    public int moneyBag;
+    public Text moneyBagText;
+
+    public void IncreaseMoney(int valueOfItem)
+    {
+        moneyBag+= valueOfItem;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        moneyBag = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyBagText.text = moneyBag.ToString();
+    }
+}
