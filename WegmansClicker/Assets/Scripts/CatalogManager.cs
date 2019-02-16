03265248ccb8e22aa52477d637af47e3c81c841c
Seth Leyens <sel6606@rit.edu>
2019-02-16 15:45:25 -0500

    Began working on catalog

---
+++ WegmansClicker/Assets/Scripts/CatalogManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatalogManager : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject[] panels;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void SelectTab(int tab)
+    {
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            if(i == tab)
+            {
+                buttons[i].interactable = false;
+                panels[i].SetActive(true);
+            }
+            else
+            {
+                buttons[i].interactable = true;
+                panels[i].SetActive(false);
+            }
+        }
+    }
+}
