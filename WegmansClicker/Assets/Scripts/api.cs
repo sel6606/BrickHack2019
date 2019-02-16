313cc8af50c0590efabdcd3335ccb39ee9d2b4d0
crb7054 <crb7054@rit.edu>
2019-02-16 15:18:37 -0500

    create api script

---
+++ WegmansClicker/Assets/Scripts/api.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Dairy, Produce, Meat, Pasta
+// Name, Brand, Price
+
+public class api {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
