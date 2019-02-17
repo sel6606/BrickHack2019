77dddafd7c4343af241c50e1b2dd650f81733b5c
LN <ln4269@rit.edu>
2019-02-16 19:16:18 -0500

    Moving front layer of clouds

---
+++ WegmansClicker/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatingBackground : MonoBehaviour
+{
+
+    float scrollSpeed = -1f;
+    Vector2 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    //Update runs once per frame
+    private void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 20);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+    
+}
