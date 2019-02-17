db9da6b35f75f392c1be441abb19f8e944025be2
Seth Leyens <sel6606@rit.edu>
2019-02-17 08:45:41 -0500

    Fixed some buggos

--- WegmansClicker/Assets/Scripts/CatalogManager.cs
+++ WegmansClicker/Assets/Scripts/CatalogManager.cs
@@ -20,7 +20,7 @@ public class CatalogManager : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        OpenCatalog();
+        //OpenCatalog();
 	}
 	
 	// Update is called once per frame
@@ -63,60 +63,57 @@ public class CatalogManager : MonoBehaviour
 
     public void InitButtonsDebug(int tab)
     {
-        switch(tab)
+        if (catalog.activeSelf)
         {
-            case 0:
-                StartCoroutine(Api.instance.GetDairyFoodItems());
-                break;
-            case 1:
-                return;
-                break;
-            case 2:
-                return;
-                break;
-            case 3:
-                return;
-                break;
-            case 4:
-                return;
-                break;
-            default:
-                return;
-                break;
-        }
-
-        currentFood = Api.instance.foodItems;
-
-        foreach (Transform child in panels[tab].transform.GetChild(0))
-        {
-            Destroy(child.gameObject);
-        }
-        int numRows = currentFood.Count / 2;
-
-        GameObject currentRow = null;
-        for (int i = 0; i < currentFood.Count; i++)
-        {
-            if (i % 2 == 0)
+            switch (tab)
             {
-                currentRow = Instantiate(rowPrefab, panels[tab].transform.GetChild(0));
-                currentRow.transform.localPosition = new Vector3(currentRow.transform.localPosition.x, currentRow.transform.localPosition.y - (100 * i), currentRow.transform.localPosition.z);
-
-                currentRow.transform.GetChild(0).GetComponentInChildren<Text>().text = currentFood[i].name + "\n$" + currentFood[i].price;
-
-                currentRow.transform.GetChild(0).gameObject.AddComponent<CatalogItem>();
-                currentRow.transform.GetChild(0).GetComponent<CatalogItem>().Food = currentFood[i];
-                currentRow.transform.GetChild(0).GetComponent<CatalogItem>().category = categories[tab];
-
-                currentRow.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(delegate { currentRow.transform.GetChild(0).GetComponent<CatalogItem>().MakePurchase(); });
+                case 0:
+                    StartCoroutine(Api.instance.GetDairyFoodItems());
+                    break;
+                case 1:
+                    return;
+                    break;
+                case 2:
+                    return;
+                    break;
+                case 3:
+                    return;
+                    break;
+                case 4:
+                    return;
+                    break;
+                default:
+                    return;
+                    break;
             }
-            else
-            {
-                currentRow.transform.GetChild(1).GetComponentInChildren<Text>().text = currentFood[i].name + "\n$" + currentFood[i].price;
-                currentRow.transform.GetChild(1).gameObject.AddComponent<CatalogItem>();
-                currentRow.transform.GetChild(1).GetComponent<CatalogItem>().Food = currentFood[i];
-                currentRow.transform.GetChild(1).GetComponent<CatalogItem>().category = categories[tab];
 
-                currentRow.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate { currentRow.transform.GetChild(1).GetComponent<CatalogItem>().MakePurchase(); });
+            currentFood = Api.instance.foodItems;
+
+            foreach (Transform child in panels[tab].transform.GetChild(0))
+            {
+                Destroy(child.gameObject);
+            }
+            int numRows = currentFood.Count / 2;
+
+            GameObject currentRow = null;
+            for (int i = 0; i < currentFood.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    currentRow = Instantiate(rowPrefab, panels[tab].transform.GetChild(0));
+                    currentRow.transform.localPosition = new Vector3(currentRow.transform.localPosition.x, currentRow.transform.localPosition.y - (100 * i), currentRow.transform.localPosition.z);
+
+                    currentRow.transform.GetChild(0).GetComponentInChildren<Text>().text = currentFood[i].name + "\n$" + currentFood[i].price;
+
+                    currentRow.transform.GetChild(0).GetComponent<CatalogItem>().Food = currentFood[i];
+                    currentRow.transform.GetChild(0).GetComponent<CatalogItem>().category = categories[tab];
+                }
+                else
+                {
+                    currentRow.transform.GetChild(1).GetComponentInChildren<Text>().text = currentFood[i].name + "\n$" + currentFood[i].price;
+                    currentRow.transform.GetChild(1).GetComponent<CatalogItem>().Food = currentFood[i];
+                    currentRow.transform.GetChild(1).GetComponent<CatalogItem>().category = categories[tab];
+                }
             }
         }
     }
