fe4186f28009ec85e4764055becd447dcbc91043
Seth Leyens <sel6606@rit.edu>
2019-02-16 19:28:01 -0500

    Committing changes in preparation for merge

--- WegmansClicker/Assets/Scripts/CatalogManager.cs
+++ WegmansClicker/Assets/Scripts/CatalogManager.cs
@@ -7,8 +7,9 @@ public class CatalogManager : MonoBehaviour
 {
     public Button[] buttons;
     public GameObject[] panels;
+    public GameObject rowPrefab;
 
-
+    public int debugButtons;
 
 	// Use this for initialization
 	void Start ()
@@ -35,6 +36,21 @@ public class CatalogManager : MonoBehaviour
                 buttons[i].interactable = true;
                 panels[i].SetActive(false);
             }
+        }
+    }
+
+    public void InitButtons(int tab)
+    {
+        foreach(Transform child in panels[tab].transform.GetChild(0))
+        {
+            Destroy(child.gameObject);
+        }
+        int numRows = debugButtons / 2;
+
+        for(int i = 0; i < numRows; i++)
+        {
+            GameObject temp = Instantiate(rowPrefab, panels[tab].transform.GetChild(0));
+            temp.transform.localPosition = new Vector3(temp.transform.localPosition.x, temp.transform.localPosition.y - (100 * i), temp.transform.localPosition.z);
         }
     }
 }
